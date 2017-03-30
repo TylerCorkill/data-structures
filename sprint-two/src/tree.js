17296ad3a12395f4269e3d69ed748036cd1c1d5d
TylerCorkill <TylerCorkill@gmail.com>
2017-03-30 15:58:44 -0700

    all tests for tree pass

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,31 @@
+require(['underscore']);
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
-
-  return newTree;
+  return _.extend(newTree, treeMethods);
 };
+
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  if (this.children.length) { return this._search(target, this); }
+  return false;
+};
+
+treeMethods._search = function(target, node) {
+  for (var child of node.children) {
+    if (child.value === target) { return true; }
+    if (child.children.length && this._search(target, child)) { return true; }
+  }
+  return false;
 };
 
 

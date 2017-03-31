534688e3eb94223757009118db96d370fd4e71e2
TylerCorkill <TylerCorkill@gmail.com>
2017-03-30 21:11:17 -0700

    hash table passes all tests but hash function collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,20 +3,29 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._collisions = {};
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.set(index, value);
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  return this._storage.get(index);
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  this._storage.each( function(value, i, storage) {
+    if (i === index) { storage[i] = undefined; }
+  });
 };
 
+// callback(value, index, storage)
+
+// [undef, undef, undef, 5, undef, 7, undef, undef]
 
 
 /*

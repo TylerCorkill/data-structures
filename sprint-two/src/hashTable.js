2b7636ef1997ee138038c55f3eceb52c443c113a
TylerCorkill <TylerCorkill@gmail.com>
2017-03-31 16:00:13 -0700

    hash table passes all resizing tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,6 +5,7 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(key, value) {
+  // debugger;
   var index = getIndexBelowMaxForKey(key, this._limit);
   var bucket = this._storage.get(index);
   if (bucket) {
@@ -15,11 +16,11 @@ HashTable.prototype.insert = function(key, value) {
       }
     }
     bucket.push([key, value]);
-    this.size++;
+    this.changeSize(1);
   } else {
     let bucket = [[key, value]];
     this._storage.set(index, bucket);
-    this.size++;
+    this.changeSize(1);
   }
 };
 
@@ -41,7 +42,7 @@ HashTable.prototype.remove = function(key) {
     for (var i = 0; i < bucket.length; i++) {
       if (bucket[i][0] === key) {
         bucket.splice(i, 1);
-        this.size--;
+        this.changeSize(-1);
         break;
       }
     }
@@ -49,19 +50,67 @@ HashTable.prototype.remove = function(key) {
       this._storage.each( function(value, i, storage) {
         if (i === index) {
           storage[i] = undefined;
-          this.size--;
+          HashTable.prototype.changeSize(-1);
         }
       });
     }
   }
 };
 
-HashTable.prototype.doubleLimit = function() {
+HashTable.prototype.changeSize = function(num) {
+  this.size += num;
+  var ratio = this.size / this._limit;
+  if (ratio >= 0.75) {
+    this.doubleLimit();
+  } else if (ratio <= 0.25) {
+    this.halveLimit();
+  }
+};
 
+
+HashTable.prototype.doubleLimit = function() {
+  // LimitedArray(this._limit);
+  var oldLimitedArray = [];
+  this._storage.each( function(value, i, storage) {
+    oldLimitedArray.push(value);
+  });
+
+  this._limit *= 2;
+  this._storage = LimitedArray(this._limit);
+
+  for (var i = 0; i < oldLimitedArray.length; i++) {
+    var bucket = oldLimitedArray[i];
+    if (bucket) {
+      for (var tuple of bucket) {
+        var key = tuple[0];
+        var value = tuple[1];
+        this.insert(key, value);
+        this.size--;
+      }
+    }
+  }
 };
 
 HashTable.prototype.halveLimit = function() {
+  var oldLimitedArray = [];
+  this._storage.each( function(value, i, storage) {
+    oldLimitedArray.push(value);
+  });
 
+  this._limit *= 0.5;
+  this._storage = LimitedArray(this._limit);
+
+  for (var i = 0; i < oldLimitedArray.length; i++) {
+    var bucket = oldLimitedArray[i];
+    if (bucket) {
+      for (var tuple of bucket) {
+        var key = tuple[0];
+        var value = tuple[1];
+        this.insert(key, value);
+        this.size--;
+      }
+    }
+  }
 };
 
 /*

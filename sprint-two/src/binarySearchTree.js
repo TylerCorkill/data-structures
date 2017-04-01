3d642a2f4df2114bf30631f1b0d2a3d8c0d45239
TylerCorkill <TylerCorkill@gmail.com>
2017-03-31 20:15:40 -0700

    all tests pass, including resizing hash tables

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,54 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null; // always less than this.value
+  this.right = null; // always greater than this.value
+
 };
 
+
+BinarySearchTree.prototype.insert = function(newVal) {
+  // this.insert.call(this.right, val)
+  if (newVal !== this.value) {
+    if (newVal < this.value) {
+      if (this.left) {
+        this.left.insert(newVal);
+      } else {
+        this.left = new BinarySearchTree(newVal);
+      }
+    } else {
+      if (this.right) {
+        this.right.insert(newVal);
+      } else {
+        this.right = new BinarySearchTree(newVal);
+      }
+    }
+  }
+
+};
+
+
+BinarySearchTree.prototype.contains = function(val) {
+  if (val === this.value) {
+    return true;
+  } else if (val < this.value) { // left
+    if (!this.left) { return false; }
+    return this.left.contains(val);
+  } else { // right
+    if (!this.right) { return false; }
+    return this.right.contains(val);
+  }
+};
+
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  cb(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(cb);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(cb);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
